80428467d945666dcbd8b1056780900a298b39e4
Ebubekir <83969385+ebubekir-pulat@users.noreply.github.com>
2025-05-03 08:06:50 +0930

    Update spines.py

--- lib/matplotlib/spines.py
+++ lib/matplotlib/spines.py
@@ -203,7 +203,7 @@ class Spine(mpatches.Patch):
     def _ensure_position_is_set(self):
         if self._position is None:
             # default position
-            self._position = ('outward', 0.0)  # in points
+            self._position = ('outward', mpl.rcParams[f'axes.spines.{self.spine_type}_outward'])  # in points
             self.set_position(self._position)
 
     def register_axis(self, axis):
